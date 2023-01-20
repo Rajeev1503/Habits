cc87f4705d0372a5112e020bb5f2844bd5395f03
Rajeev Kumar <rk.rajeevkr1503@gmail.com>
2023-01-20 20:18:47 +0530

    new changes gssp 14

--- pages/app.js
+++ pages/app.js
@@ -129,7 +129,7 @@ export async function getServerSideProps(ctx) {
   // }
 
  const data = await fetchHelper(
-    `https://taskify-app-amber.vercel.app/api/userId/tasklisttypes`,
+    `${NEXT_URL}/api/userId/tasklisttypes`,
     "GET"
   )
     const allTaskListTypes = JSON.parse(data)
