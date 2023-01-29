9d33530ed0e4167f396892def8c574c9abf6428b
Rajeev Kumar <rk.rajeevkr1503@gmail.com>
2023-01-29 21:13:37 +0530

    loading added v7

--- src/components/Navigation/NavLinks.js
+++ src/components/Navigation/NavLinks.js
@@ -12,43 +12,63 @@ const NavLinks = (props) => {
   const backgroundColorContext = useContext(BackgroundColorContext);
   return (
     <div className="w-full flex flex-row justify-between items-center font-semibold text-sm">
-      <div className="w-1/6 text-center flex justify-start pl-8 items-center pt-2">
+      <div
+        className="text-center flex justify-start pl-8 items-center pt-2"
+        style={{ width: "12%" }}
+      >
         <Link href="/">
           <h2>TODO APP</h2>
         </Link>
       </div>
-      <div className="w-1/5 text-center flex justify-start">
+      <div className="border-l border-border-dark text-center flex justify-start px-4" style={{ width: "20%" }}>
         {currentTaskListTypeContext?.currentTaskListType?.name}
       </div>
 
-      <div className="w-1/5 text-center flex justify-start capitalize">
-        {taskListContext?.taskList?.taskListName}
+      <div
+        className="border-l border-r border-border-dark text-center flex justify-between capitalize px-4"
+        style={{ width: "43%" }}
+      >
+        <div>{taskListContext?.taskList?.taskListName}</div>
+        <div>
+          <div className="text-xs border border-border-dark rounded-lg cursor-pointer max-w-full p-1 px-2">
+            <FaBell size={16} />
+          </div>
+        </div>
       </div>
 
-      <div className="w-1/5">
-        <ul className="flex flex-row justify-center items-center gap-2 text-center">
-          <li className="text-xs border border-border-dark rounded-lg cursor-pointer max-w-full p-1 px-2">
-            <Link href="#">
-              <FaBell size={16}/>
-            </Link>
-          </li>
-        </ul>
-      </div>
-
-      <div className="w-1/5">
-        <ul className="flex flex-row justify-center items-center gap-2 text-center">
-         
-          <li className="text-xs flex gap-1 items-center border border-border-dark rounded-lg max-w-max p-1 cursor-pointer" onClick={()=>{backgroundColorContext.backgroundColorDispatch({type:'LIGHT'})}}>
-            <p><span>Color Mode : </span>
-            <span className={`${(backgroundColorContext.backgroundColorState.mode === 'dark')? 'text-green-400': 'text-red-400'}`}>{backgroundColorContext.backgroundColorState.mode}</span>
+      <div className="" style={{ width: "25%" }}>
+        <ul className="w-full flex flex-row justify-end items-center gap-2">
+          <li
+            className="text-xs flex gap-1 items-center border border-border-dark rounded-lg max-w-max p-1 cursor-pointer"
+            onClick={() => {
+              backgroundColorContext.backgroundColorDispatch({ type: "LIGHT" });
+            }}
+          >
+            <p>
+              <span>Color Mode : </span>
+              <span
+                className={`${
+                  backgroundColorContext.backgroundColorState.mode === "dark"
+                    ? "text-green-400"
+                    : "text-red-400"
+                }`}
+              >
+                {backgroundColorContext.backgroundColorState.mode}
+              </span>
             </p>
           </li>
-          <li className="text-xs flex gap-1 items-center border border-border-dark rounded-lg max-w-max p-1 cursor-pointer" onClick={()=>{backgroundColorContext.backgroundColorDispatch({type:'LIGHT'})}}>
-            <p><span>Custom Color Picker </span>
-            <span></span>
+          <li
+            className="text-xs flex gap-1 items-center border border-border-dark rounded-lg max-w-max p-1 cursor-pointer"
+            onClick={() => {
+              backgroundColorContext.backgroundColorDispatch({ type: "LIGHT" });
+            }}
+          >
+            <p>
+              <span>Custom Color Picker </span>
+              <span></span>
             </p>
           </li>
-           <li className="text-xs border border-border-dark rounded-lg cursor-pointer max-w-max p-1 px-2">
+          <li className="text-xs border border-border-dark rounded-lg cursor-pointer max-w-max p-1 px-2">
             {status === "loading" ? (
               "loading"
             ) : session?.user ? (
